b0ce84f0cd09fa877f37144762fbf7c4a474d3f5
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-06-10 22:39:05 -0300

    :white_check_mark: feat: improve test

--- go/ola_test.go
+++ go/ola_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola()
-	esperado := "Olá, mundo"
+	resultado := Ola("spanish")
+	esperado := "Olá, spanish"
 
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)

4ecd150169572317d09657c2d27e397d7de05fa7
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-06-11 22:47:30 -0300

    :white_check_mark: feat: improve test

--- go/ola.go
+++ go/ola.go
@@ -5,9 +5,13 @@ import "fmt"
 const constOla = "Olá, " // <- Isso é uma declaração de uma constante
 
 func Ola(name string) string {
+	if name == "" {
+		name = "mundo"
+	}
 	return constOla + name
 }
 
 func main() {
-	fmt.Println(Ola("mundo"))
+	fmt.Println(Ola("spanish"))
+	fmt.Println(Ola(""))
 }

e1b396b118e163eed83e28e3e4ce12eb8c93f869
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-06-11 22:45:14 -0300

    :test_tube: feat: failing test

--- go/ola_test.go
+++ go/ola_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("spanish")
-	esperado := "Olá, spanish"
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-	}
+	t.Run("Diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("spanish")
+		esperado := "Olá, spanish"
+
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	})
+
+	t.Run("Diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	})
 }

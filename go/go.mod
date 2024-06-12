192966474816b2d938b41a47671d8b325c09b0af
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2024-06-11 22:49:10 -0300

    :recycle: feat: improve test validation

--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,3 @@
 module go/estudos
 
-go 1.22.3
+go 1.22.4
--- go/ola_test.go
+++ go/ola_test.go
@@ -3,22 +3,24 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	}
 
 	t.Run("Diz olá para as pessoas", func(t *testing.T) {
 		resultado := Ola("spanish")
 		esperado := "Olá, spanish"
 
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("Diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
 		resultado := Ola("")
 		esperado := "Olá, mundo"
 
-		if resultado != esperado {
-			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
-		}
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 }
